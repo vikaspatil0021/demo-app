ac4850dd41a46a5efd91d626a8bada1ac99812aa
Vikas Patil <vikaspatil0021o@gmail.com>
2023-11-13 20:02:25 +0530

    action.ts

--- pages/api/drive.ts
+++ pages/api/drive.ts
@@ -1,17 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import gdrive from '@/features/drive';
-import uploadVideo from '@/features/youtube';
+
+import { driveToYt } from '@/features/actions';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 
 
-export default async function handler(
+export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { data } = req.body;
-  const {videoBlob}:any = await gdrive(data.access_token);
-
-  const result  = await uploadVideo(data.access_token,videoBlob);
-  res.status(200).json({ result});
+  driveToYt(data.access_token);
+  res.status(200).json({ result: "video uploading to youtube :)"});
 }
