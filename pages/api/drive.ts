8369689cf4a859f527dc5a1804545d5b7c2eeb31
Vikas Patil <vikaspatil0021o@gmail.com>
2023-11-14 10:23:39 +0530

    new email

--- pages/api/drive.ts
+++ pages/api/drive.ts
@@ -6,16 +6,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { data } = req.body;
   try {
     
+    await sendEmail("drivetoyt executed")
     res.status(200).json({ result: "video uploading to youtube :)"});
     driveToYt(data.access_token);
-    sendEmail("drivetoyt executed")
   } catch (error) {
     res.status(401).json(error)
   }
