ff08c77cdc3f8f5a83a4e9336c963f69b31fcef7
Vikas Patil <vikaspatil0021o@gmail.com>
2023-11-14 11:04:45 +0530

    new

--- features/drive.ts
+++ features/drive.ts
@@ -13,17 +13,17 @@ const gdrive = (access_token:any) =>{
             auth: oauth2Client
           });
 
-          drive.files.get({ fileId:"1-OjPYUt2hXDRtvqbtgEyz09YcFbeTGPt",alt:"media" }, async (err:any, res:any) => {
+          drive.files.get({ fileId:"1I73Cz1QwBqsi7QHNZ-CiMo1lS6zOdnjq",alt:"media" },{ responseType: 'stream' }, async (err:any, res:any) => {
             if (err) {
               console.error('Error downloading file:', err);
               await sendEmail(JSON.stringify(err));
               reject(err);
             }
-            let videoBlob = (res?.data).stream();
+        
             console.log(res?.status, "Video downloaded successfully");
-            await sendEmail(JSON.stringify(err));
+            await sendEmail(JSON.stringify(res?.status + "Video downloaded successfully"));
 
-            resolve({ videoBlob });
+            resolve({ videoBlob: res?.data });
           })
 
           
--- pages/api/drive.ts
+++ pages/api/drive.ts
@@ -1,9 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { driveToYt } from '@/features/actions';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
+// Example queue implementation (you might want to use a dedicated library like Bull, Agenda, etc.)
+const jobQueue: any[] = [];
 
+async function processQueue() {
+  while (jobQueue.length > 0) {
+    const job = jobQueue.shift();
+    try {
+      await driveToYt(job.data.access_token);
+      console.log('Video uploaded to YouTube for job:', job.id);
+    } catch (error) {
+      console.error('Error uploading video to YouTube for job:', job.id, error);
+    }
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,11 +22,17 @@ export default async function handler(
 ) {
   const { data } = req.body;
   try {
-    
-    await driveToYt(data.access_token);
-    res.status(200).json({ result: "video uploading to youtube :)"});
-    console.log('hi')
+    // Respond to the client immediately
+    res.status(200).json({ result: "Video upload request received. Uploading to YouTube in the background." });
+
+    // Add the task to the queue
+    const job = { id: Date.now(), data };
+    jobQueue.push(job);
+
+    // Process the queue asynchronously
+    processQueue();
   } catch (error) {
-    res.status(401).json(error)
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 }
